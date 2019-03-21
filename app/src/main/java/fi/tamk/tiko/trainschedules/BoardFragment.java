9d8a1658dd00e9c07f902ca66316498f3e22e2c2
Haataja <hanna.haataja@cs.tamk.fi>
2019-03-21 22:54:11 +0200

    Added refreshing to the BoardFragment

--- app/src/main/java/fi/tamk/tiko/trainschedules/BoardFragment.java
+++ app/src/main/java/fi/tamk/tiko/trainschedules/BoardFragment.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class BoardFragment extends Fragment {
+    private SwipeRefreshLayout srl;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.board_view, container, false);
+        srl = view.findViewById(R.id.swiperefreshBoard);
+        srl.setOnRefreshListener(() -> doSomething());
         return view;
+    }
+
+    private void doSomething() {
+        Log.i(this.getClass().getName(), "Refreshing!!");
+        srl.setRefreshing(false);
     }
 }
