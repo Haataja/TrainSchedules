a9c9c4fd689ef4189bd3ab7b0b2b8275f30a5d30
Haataja <hanna.haataja@cs.tamk.fi>
2019-03-22 13:14:37 +0200

    Fixed search

--- app/src/main/java/fi/tamk/tiko/trainschedules/MainActivity.java
+++ app/src/main/java/fi/tamk/tiko/trainschedules/MainActivity.java
@@ -2,15 +2,45 @@ package fi.tamk.tiko.trainschedules;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity implements ChosenStation {
-
+    private Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(this.getClass().getName(), "Loading menus");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        Log.d(this.getClass().getName(), "Search item " + searchItem);
+
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        this.menu = menu;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -25,5 +55,16 @@ public class MainActivity extends AppCompatActivity implements ChosenStation {
             intent.putExtra("name", stationCode);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        boolean result = false;
+        if(item.getItemId() == R.id.search){
+            result = true;
+        }
+        return result;
     }
 }
